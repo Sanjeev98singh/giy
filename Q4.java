504e34d46b069861b3383a887fa92131ce69bda9
Sanjeev <coolviccy123@gmail.com>
2022-10-28 17:28:49 +0530

    first commit

---
+++ Q4.java
@@ -0,0 +1,68 @@
+package assignment4;
+
+import jdk.internal.util.xml.impl.Input;
+
+
+class scanText{
+    String SourceFilePath;
+    HashMap<Integer, ArrayList<String>> words ;
+    public scanText(String sourceFilePath) {
+        SourceFilePath = sourceFilePath;
+        words= new HashMap<>();
+        UpdateMap();
+    }
+
+    private void UpdateMap(){
+        try {
+            FileReader fileReader = new FileReader(SourceFilePath);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line=bufferedReader.readLine();
+            while (line!=null){
+                    ArrayList<String> val = words.getOrDefault(line.length(),new ArrayList<String>());
+                    val.add(line);
+                    words.put(line.length(),val);
+                line= bufferedReader.readLine();
+            }
+
+            bufferedReader.close();
+            fileReader.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void outputWordFrequency(String path){
+
+        try {
+            for (Map.Entry<Integer,ArrayList<String>> entry :
+                words.entrySet()) {
+                System.out.println( entry.getKey() + " length words are "+ entry.getValue().size());
+                FileWriter fileWriter = new FileWriter(path+"/words"+entry.getKey()+".txt");
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+                for (String word:
+                        entry.getValue()) {
+                    bufferedWriter.write(word);
+                    bufferedWriter.newLine();
+                }
+                bufferedWriter.close();
+                fileWriter.close();
+            }
+          
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+public class Q4 {
+    public static void main(String[] args) {
+        scanText sc = new scanText("src/assignment4/words.txt");
+        sc.outputWordFrequency("src/assignment4");
+    }
+}
